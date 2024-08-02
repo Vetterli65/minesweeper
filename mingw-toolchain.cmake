12c108b09a0bd1cc10a63bc6bfed4b266bc1d752
vetterli <abatter42@gmail.com>
2024-08-02 18:00:09 -0500

    Updated windows compilation

--- mingw-toolchain.cmake
+++ mingw-toolchain.cmake
@@ -8,10 +8,8 @@ set(CMAKE_RC_COMPILER /usr/bin/x86_64-w64-mingw32-windres)
 set(CMAKE_FIND_ROOT_PATH /usr/x86_64-w64-mingw32)
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
-# Force static linking
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -static")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -static")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -static -static-libgcc -static-libstdc++")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -static -static-libgcc -static-libstdc++")
